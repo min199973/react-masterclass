275f3b84ed5f4a99024403d722f8820c60831526
min199973 <min199973@gmail.com>
2023-04-05 14:42:12 +0900

    ReactMaster #3.0 ~ #3.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const Hi = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+function Dummy({ text, active = false }: DummyProps) {
+  return <h1>{text}</h1>;
+}
 function App() {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Dummy text="Hello"></Dummy>
+      <button onClick={onClick}>click me</button>
+    </Container>
   );
 }
 
